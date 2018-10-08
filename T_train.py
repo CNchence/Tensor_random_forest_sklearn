b883c7f27acec8ea849b1b96ff814ba9469d3436
leaguc <364437360@qq.com>
2018-10-08 21:28:14 +0800

    add T data gene and model train
    add T data gene and model train

---
+++ T_train.py
@@ -0,0 +1,22 @@
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+
+res = np.loadtxt("F:\\D2CO_dataset\\detect_train_data\\1538123396_pos_neg\\data.txt", delimiter=" ")
+res = res[1:len(res)]
+res = np.split(res, 2, axis=1)[1]
+print(res)
+data = np.loadtxt("F:\\D2CO_dataset\\detect_train_data\\1538123396_pos_neg\\position.txt", delimiter=" ")
+data = data[1:len(data)]
+
+clf = RandomForestRegressor(n_estimators = 500, max_depth = 10, oob_score = True,random_state = 10, verbose = 10)
+
+t_size=0.1#测试集所占比例，分数
+train_X, test_X, train_Y, test_Y = train_test_split(data, res, test_size=t_size, random_state=10)
+
+clf.fit(train_X,train_Y)
+
+y_result = clf.predict(test_X)
+print(y_result)
+print(test_Y)
---
+++ gene_ranger_data.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals import joblib
+from sklearn.model_selection import train_test_split
+
+res = np.loadtxt("F:\\D2CO_dataset\\detect_train_data\\1538123396_pos_neg\\data.txt", delimiter=" ")
+res = res[1:len(res)]
+res = np.split(res, 2, axis=1)[1]
+res = np.split(res, 3, axis=1)
+print(res)
+data = np.loadtxt("F:\\D2CO_dataset\\detect_train_data\\1538123396_pos_neg\\position.txt", delimiter=" ")
+data = data[1:len(data)]
+dic = {0: "tx", 1: "ty", 2: "tz"}
+for i in range(0, len(res)):
+    name_list =np.array([["%s" % dic[i], "minx", "miny", "maxx", "maxy"]])
+    ranger_data = np.concatenate((res[i], data), axis=1)
+    ranger_data = np.concatenate((name_list, ranger_data), axis=0)
+    print("ranger_data:", ranger_data)
+    np.savetxt('F:\\D2CO_dataset\\detect_train_data\\1538123396_pos_neg\\ranger_train_%s.dat' % dic[i], ranger_data, fmt='%s',)
